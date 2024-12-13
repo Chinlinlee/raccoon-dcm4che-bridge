7b41ac116db43685fdd53cb12d55058b2924c104
CHIN\a5566 <a5566qq2581@gmail.com>
2024-12-13 13:19:03 +0800

    feat: update dcm2jpg to v1.0.1 for handling more opts

--- src/index.js
+++ src/index.js
@@ -8,7 +8,13 @@ const path = require("path");
  * @returns 
  */
 module.exports.raccoonDcm4cheJavaLoader = function(jvmOptions={}) {
-    java.ensureJvm(jvmOptions);
+    let imageApiOpts = ["--add-opens=java.desktop/javax.imageio.stream=ALL-UNNAMED", "--add-opens=java.base/java.io=ALL-UNNAMED"];
+    java.ensureJvm({
+        ...jvmOptions,
+        opts: Array.isArray(jvmOptions.opts) ? [
+            ...jvmOptions.opts, ...imageApiOpts
+        ]: imageApiOpts
+    });
 
     let jarFiles = glob.sync("**/*.jar", {
         cwd: path.join(__dirname, "./javaNode"),
