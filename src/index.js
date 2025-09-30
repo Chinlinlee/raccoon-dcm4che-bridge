114e96716cd57a8b00bdfbd71fa60ec8544d071c
chinlinlee <a5566qq2581@gmail.com>
2025-10-01 02:03:47 +0800

    refactor: apply singleton to java instance

--- src/index.js
+++ src/index.js
@@ -2,27 +2,51 @@ const java = require('java-bridge');
 const glob = require("glob");
 const path = require("path");
 
+let jvmInitialized = false;
+let javaInstance = null;
+
+
 /**
  * 
  * @param {import("java-bridge").JVMOptions} jvmOptions 
  * @returns 
  */
 module.exports.raccoonDcm4cheJavaLoader = function(jvmOptions={}) {
-    let imageApiOpts = ["--add-opens=java.desktop/javax.imageio.stream=ALL-UNNAMED", "--add-opens=java.base/java.io=ALL-UNNAMED"];
-    java.ensureJvm({
-        ...jvmOptions,
-        opts: Array.isArray(jvmOptions.opts) ? [
-            ...jvmOptions.opts, ...imageApiOpts
-        ]: imageApiOpts
-    });
+    if (jvmInitialized) {
+        return javaInstance;
+    }
 
-    let jarFiles = glob.sync("**/*.jar", {
-        cwd: path.join(__dirname, "./javaNode"),
-        absolute: true
-    });
+    let imageApiOpts = ["--add-opens=java.desktop/javax.imageio.stream=ALL-UNNAMED", "--add-opens=java.base/java.io=ALL-UNNAMED"];
+
+    try {
+        java.ensureJvm({
+            ...jvmOptions,
+            opts: Array.isArray(jvmOptions.opts) ? [
+                ...jvmOptions.opts, ...imageApiOpts
+            ]: imageApiOpts
+        });
     
-    java.appendClasspath(jarFiles);
-    
-    return java;
+        let jarFiles = glob.sync("**/*.jar", {
+            cwd: path.join(__dirname, "./javaNode"),
+            absolute: true
+        });
+        
+        java.appendClasspath(jarFiles);
+
+        javaInstance = java;
+        jvmInitialized = true;
+        
+        return java;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
 
+module.exports.isJvmInitialized = function () {
+    return jvmInitialized;
+};
+module.exports.resetJvmState = function() {
+    jvmInitialized = false;
+    javaInstance = null;
+}
